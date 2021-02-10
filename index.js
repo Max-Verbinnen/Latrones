ba5b2905dd116785fc571fb0dc0b6c70d798dd88
Max <you@example.com>
2021-02-10 20:37:21 +0100

    only send move to specific room

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ io.on("connection", client => {
   function handleMove({html, capture}) {
     let roomName = clientRooms[client.id];
     state[roomName] = html;
-    client.broadcast.emit("move", {html: state[roomName], capture: capture});
+    io.sockets.in(roomName).broadcast.emit("move", {html: state[roomName], capture: capture});
   }
 
   function handleWinner(winner) {
